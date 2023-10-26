6551a96ac5b632a08c3369375939808939eaffab
pratham <prathamchande@gmail.com>
2023-10-27 01:12:10 +0530

    swiper test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LanguageService } from './service/common/language.service';
+import { register } from 'swiper/element/bundle';
 
+register();
 
 @Component({
   selector: 'app-root',
